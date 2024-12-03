fb61ae48c99a24ecce73f98f70ac1b12b3a6f1be
hurat-ashraf <huratashraf313@gmail.com>
2024-12-03 13:25:07 +0000

    added use of switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+
+//       break;
+
+//       default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "february":
+        console.log("february");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "march":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
