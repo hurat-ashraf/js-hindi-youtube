0be497fb899f35c97804e05ce5f7ece61c586ae9
hurat-ashraf <huratashraf313@gmail.com>
2024-12-16 11:06:55 +0000

    loops discussion

---
+++ 05_iterations/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: 'jabascript',
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {    
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+// Maps   
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "Unites states of america")
+// map.set('fr', "france")
+// map.set('IN', "india")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
